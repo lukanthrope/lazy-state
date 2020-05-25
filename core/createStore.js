b2390f00a437fe3f902962689dd61a464465e985
lukanthrope <bydlocoder228@gmail.com>
2020-05-25 22:37:38 +0300

    kinda works

---
+++ core/createStore.js
@@ -0,0 +1,36 @@
+function createStore(initialState, reducer) {
+    let state = initialState;
+    let listeners = new Set();
+
+    function dispatch(action) {
+        state = reducer(state, action);
+    }
+
+    function inform(data) {
+        listeners.forEach(setter => {
+            setter(data);
+        });
+    }
+
+    function setState(newState) {
+        state = newState;
+        inform(newState);
+    }
+
+    function getState() {
+        return state;
+    }
+
+    function subscribe(s) {
+        listeners.add(s);
+    }
+
+    return {
+        setState,
+        getState,
+        subscribe,
+        dispatch,
+    };
+}
+
+export default createStore;
